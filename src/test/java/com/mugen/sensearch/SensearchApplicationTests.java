73c7fa92751c7b2fb4e3dc64c4f4b04e0b787caf
housseinmomo <smrobla34@gmail.com>
2022-11-02 19:50:58 +0100

    initial commit

---
+++ src/test/java/com/mugen/sensearch/SensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugen.sensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
